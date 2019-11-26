b336f60d16af9012907fd51c18efc48f1bedf154
LilTomasG <tomaskosta99@gmail.com>
2019-11-26 20:08:04 +0000

    ainda nao mas ja cheira a sucesso

--- proj2/file-receiver.c
+++ proj2/file-receiver.c
@@ -1,1 +1,98 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h> 
+#include <string.h> 
+#include <sys/types.h> 
+#include <sys/socket.h> 
+#include <arpa/inet.h> 
+#include <netinet/in.h>  
+#include "packet-format.h"
+
+int main(int argc, char const *argv[]){
+	if( argc != 4)
+		exit(-1);
+	
+	int sockfd;
+  int len; 
+	int status_packet;
+	int last_seq_num = 1;
+	const char* file_name = argv[1];
+	int port = atoi(argv[2]);	
+	int window_size = atoi(argv[3]);
+	struct sockaddr_in servaddr,cliaddr;
+	data_pkt_t packet;
+	ack_pkt_t ack;
+
+
+	// Creating socket file descriptor 
+    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0){ 
+        perror("socket creation failed"); 
+        exit(-1); 
+    } 
+     
+    memset(&servaddr, 0, sizeof(servaddr));  
+    memset(&cliaddr, 0, sizeof(cliaddr)); 
+    
+    // Filling server information 
+    servaddr.sin_family = AF_INET; // IPv4 
+    servaddr.sin_addr.s_addr = INADDR_ANY; 
+    servaddr.sin_port = htons(port); 
+      
+    // Bind the socket with the server address 
+    if ( bind(sockfd, (const struct sockaddr *)&servaddr, sizeof(servaddr)) < 0) 
+    { 
+        perror("bind failed"); 
+        exit(EXIT_FAILURE); 
+    } 
+
+    // Stop-and-wait receiver 
+    FILE *fp;
+    fp = fopen(file_name, "w");
+    fseek(fp, 0 , SEEK_SET);
+    
+    for(;;){
+   		status_packet = recvfrom(sockfd, (struct data_pkt_t*)&packet, sizeof(data_pkt_t) , 0, (struct sockaddr *) &cliaddr, (socklen_t *) sizeof(cliaddr)); 
+
+        printf("%u\n", packet.seq_num);
+        printf("%s\n", packet.data);
+  
+   		
+/*
+      printf("%d\n", status_packet);
+      if(status_packet < 0){ //Recebemos um packet corrompido
+        puts("MAGIA");
+        continue;
+   		}
+  
+*/   		
+
+      if( packet.seq_num == last_seq_num){ //recebemos um packet que ja tinhamos recebido
+        sendto(sockfd, (struct ack_pkt_t *)&ack, (size_t)sizeof(ack_pkt_t), 
+        MSG_CONFIRM, (const struct sockaddr *) &cliaddr,  
+            sizeof(cliaddr)); 
+   		}
+
+   		else{  //tudo bem do nosso lado vamos mandar um ack 
+        last_seq_num = packet.seq_num;
+        ack.seq_num = packet.seq_num +1;
+        
+        fseek(fp, 1000 * (packet.seq_num - 1), SEEK_SET);
+        
+        fwrite(packet.data,1,sizeof(packet.data), fp);
+
+        puts("ENTREI");
+
+        sendto(sockfd, (struct ack_pkt_t *)&ack, (size_t)sizeof(ack_pkt_t), 
+        MSG_CONFIRM, (const struct sockaddr *) &servaddr,  
+            sizeof(servaddr));     
+   		}
+
+      if(strlen(packet.data) < 1000){
+        fclose(fp);
+        puts("ULTIMO PACKET ACABOU");
+        exit(-1 );
+      }
+    
+    }
+	return 0;
+}
--- proj2/file-sender.c
+++ proj2/file-sender.c
@@ -1,1 +1,103 @@
-#include <stdio.h>
+#include <netdb.h> 
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <string.h> 
+#include <sys/socket.h>
+#include <sys/time.h>
+#include <time.h>
+
+#include <sys/types.h> 
+#include <netinet/in.h> 
+#include <errno.h> 
+#include <arpa/inet.h> 
+#include <unistd.h> 
+
+#include "packet-format.h"
+
+#define Socket_Adress struct sockaddr
+#define SEGMENTS 1000
+
+
+data_pkt_t get_chunk(char* file_name, uint32_t seq_num, FILE * file_i) {
+    int i;
+    data_pkt_t packet;
+
+
+    fseek(file_i, 1000 * (seq_num - 1), SEEK_SET);
+    fgets(packet.data, 1000, file_i);
+    packet.seq_num = seq_num;   
+    return packet;
+}
+
+
+int main(int argc, char** argv) { 
+	int sockfd; 
+    uint32_t seq_num = 1;
+	int port = atoi(argv[3]);
+	struct sockaddr_in servaddr;
+	struct hostent * newhost = gethostbyname(argv[2]);
+    char* file_name = argv[1];
+    
+    FILE *file_i;
+    file_i = fopen(file_name, "r");
+
+    struct timeval tv;
+    tv.tv_sec = 1;
+    tv.tv_usec = 0;
+
+	sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+
+    if (sockfd == -1) { 
+		printf("Socket creation failed...\n"); 
+		exit(-1); 
+	} 
+
+    if (setsockopt(sockfd, SOL_SOCKET, SO_RCVTIMEO, &tv, sizeof(struct timeval)) < 0) {
+        perror("Socket option failed...\n"); 
+		exit(-1); 
+    }
+
+	bzero(&servaddr, sizeof(servaddr)); 
+
+	servaddr.sin_family = AF_INET; 
+	servaddr.sin_addr.s_addr = INADDR_ANY;
+	servaddr.sin_port = htons(port);
+    
+    while(1) {
+
+        int tentativas = 3;
+        clock_t t;
+        data_pkt_t packet = get_chunk(file_name, seq_num, file_i);
+        ack_pkt_t ack;
+
+        printf("%u\n", packet.seq_num);
+        printf("%s\n", packet.data);
+        
+
+        sendto(sockfd, (data_pkt_t *) &packet, sizeof(data_pkt_t), MSG_CONFIRM, (struct sockaddr *) &servaddr, sizeof(servaddr));
+
+        while (tentativas > 0){
+
+            if(recvfrom(sockfd, (ack_pkt_t *) &ack, sizeof(ack_pkt_t), 0, (struct sockaddr *) &servaddr,(socklen_t *) sizeof(servaddr)) < 0){
+                sendto(sockfd, (data_pkt_t *) &packet, sizeof(data_pkt_t), MSG_CONFIRM, ( struct sockaddr *) &servaddr, sizeof(servaddr));
+                tentativas--;
+
+                if(tentativas == 0) {
+                    fclose(file_i);
+                    close(sockfd);
+                    exit(-1);
+                }
+                continue;
+            }
+
+            break;
+        }
+
+        seq_num++;
+
+        if(strlen(packet.data) < 1000) {
+            fclose(file_i);
+	        close(sockfd); 
+        }
+    }
+} 
